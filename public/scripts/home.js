d0fadb1f4dd56c9cb96906feb075177db59b9f8d
Nikolina Kokor <nkokor@icloud.com>
2023-03-07 22:30:53 +0100

    home page changes

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,10 +1,22 @@
 let ajax = AjaxCalls;
 
-let logoutButton = document.getElementById("logout-button");
-logoutButton.addEventListener("click", function() {
-  ajax.postLogout(function(error, data) {
-    if(error == null) {
-      location.href="/login.html";
+window.onload = function() {
+  ajax.getHome(function(error, data) {
+    let warning = document.getElementById("warning");
+    if(error != null) {
+      warning.className = "on";
     }
-  })
+    else {
+      warning.className = "off";
+      let logoutButton = document.getElementById("logout-button");
+      logoutButton.addEventListener("click", function() {
+      ajax.postLogout(function(error, data) {
+      if(error == null) {
+        location.href="/login.html";
+    }
+  });
 });
+    }
+  });
+}
+
